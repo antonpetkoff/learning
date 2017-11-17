d91cba4d24ba9df6b709ddd852df256f0d631212
Anton Petkov <anton.petkoff@gmail.com>
2017-11-18 00:17:16 +0200

    FP consultation

---
+++ scheme/consultation-2017/min-sum-digit.scm
@@ -0,0 +1,36 @@
+(require rackunit rackunit/text-ui)
+
+; Задача 1. Да се напише функция (min-sum-digit a b k), която
+; намира най-малкото от целите числа от a да b,
+; чиято сума на цифрите се дели на k
+
+(define (sum-digits n)
+  (define (iter digits sum)
+    (if (= digits 0)
+        sum
+        (iter (quotient digits 10)
+              (+ sum
+                 (remainder digits 10)))))
+  (iter n 0))
+
+; (range 1 5) -> '(1 2 3 4)
+; (filter even? '(1 2 3 4)) -> '(2 4)
+; (min 9 2 3 1 4) -> 1
+; range |> map |> filter |> min
+(define (min-sum-digit a b k)
+  (define (digit-sum-divisible-by-k number)
+    (= 0 (remainder (sum-digits number) k)))
+  (let* ((numbers (range a (+ b 1)))
+         (divisible (filter digit-sum-divisible-by-k numbers)))
+    (apply min divisible)))
+
+(define min-sum-digit-tests
+  (test-suite
+    "Tests for min-sum-digit"
+
+    (check = (min-sum-digit 1 5 3) 3)
+    (check = (min-sum-digit 1 7 2) 2)
+    (check = (min-sum-digit 11 16 3) 12)
+))
+
+(run-tests min-sum-digit-tests)
