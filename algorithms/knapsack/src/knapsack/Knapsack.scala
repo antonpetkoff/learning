5348908376844ac1e1af00719b5dd2f5811341e7
Anton Petkov <anton.petkoff@gmail.com>
2017-11-26 14:25:58 +0200

    Knapsack problem: Mutate and crossover

--- algorithms/knapsack/src/knapsack/Knapsack.scala
+++ algorithms/knapsack/src/knapsack/Knapsack.scala
@@ -1,14 +1,27 @@
 package knapsack
 
+import scala.collection.mutable
 import scala.util.Random
 
 
 object KnapsackProblem extends App {
   sealed abstract class Gene
-  case class Knapsack(items: Vector[Int]) extends Gene
+  case class Knapsack(items: mutable.Buffer[Int]) extends Gene
 
   def generate(size: Int): Knapsack = {
-    Knapsack(Stream.continually(Random nextInt 2).take(size).toVector)
+    Knapsack(Stream.continually(Random nextInt 2).take(size).toBuffer)
+  }
+
+  def mutate(knapsack: Knapsack) = {
+    val index = Random.nextInt(knapsack.items.size)
+    knapsack.items.update(index, knapsack.items(index) ^ 1)
+  }
+
+  def crossover(k1: Knapsack, k2: Knapsack): Knapsack = {
+    val cutoff = k1.items.size / 2
+    val newItems = k1.items.take(cutoff)
+    newItems.appendAll(k2.items.drop(cutoff))
+    Knapsack(newItems)
   }
 
   // can we use case without match?
@@ -19,7 +32,7 @@ object KnapsackProblem extends App {
           (wSum + taken * weights(index), vSum + taken * values(index))
         }
       } match {
-        case (wSum, vSum) if wSum > capacity => vSum - 7 * (capacity - wSum)
+        case (wSum, vSum) if wSum > capacity => vSum - 7 * (wSum - capacity)
         case (_, vSum) => vSum
       }
     }
@@ -44,4 +57,15 @@ object KnapsackProblem extends App {
   println(s"knapsack = ${knapsack}")
   val knapsackFitness = fitness(knapsack)
   println(s"knapsackFitness = ${knapsackFitness}")
+  mutate(knapsack)
+  println(s"mutated knapsack = ${knapsack}")
+  println(s"mutated knapsackFitness = ${fitness(knapsack)}")
+
+  val knapsack2 = generate(itemCount)
+  println(s"knapsack2 = ${knapsack2}")
+  println(s"knapsack2Fitness = ${fitness(knapsack2)}")
+
+  val knapsack3 = crossover(knapsack, knapsack2)
+  println(s"knapsack3 aftr crossover = ${knapsack3}")
+  println(s"knapsack3 fitness = ${fitness(knapsack3)}")
 }
