0ed012105fa724e0b90f332479d5a2e3af429393
Anton Petkov <anton.petkoff@gmail.com>
2017-12-17 16:04:56 +0200

    Split k-NN into functions

--- algorithms/knn/knn.py
+++ algorithms/knn/knn.py
@@ -1,9 +1,52 @@
+from __future__ import division
 import csv
 import random
 from math import ceil, sqrt
+from collections import Counter
 
-SEED = 42
-random.seed(SEED)
+
+def read_iris(filename):
+    data = []
+    with open(filename, 'rb') as csvfile:
+        data_set = csv.reader(csvfile, delimiter=',')
+        for sample in data_set:
+            data.append(list(map(float, sample[:4])) + [sample[4]])
+    return data
+
+
+def train_test_split(data, test_size=0.2, random_seed=42):
+    random.seed(random_seed)
+    indices = range(len(data))
+    random.shuffle(indices)
+    split_pivot = int(ceil(0.2 * len(data)))   # 20 percent are test data
+    test_set = map(lambda i: data[i], indices[:split_pivot])
+    train_set = map(lambda i: data[i], indices[split_pivot:])
+    return (train_set, test_set)
+
+
+def get_features(sample):
+    return sample[:4]
+
+
+def get_label(sample):
+    return sample[4]
+
+
+def euclidian_distance(sample1, sample2):
+    return sqrt(sum(map(lambda t: (t[0] - t[1]) ** 2,
+                        zip(get_features(sample1), get_features(sample2)))))
+
+
+def predict(train_set, sample, k=3, distance=euclidian_distance):
+    k_nearest = sorted(train_set, key=lambda s: distance(s, sample))[:k]
+    return Counter(map(get_label, k_nearest)).most_common(1)[0][0]
+
+
+def accuracy(train_set, test_set, k=3):
+    matches = map(lambda s: predict(train_set, s, k) == get_label(s), test_set)
+    return Counter(matches)[True] / len(test_set)
+    # return Counter(matches)[True]
+
 
 # Attribute Information:
 # 1. sepal length in cm
@@ -14,26 +57,6 @@ random.seed(SEED)
 # -- Iris Setosa
 # -- Iris Versicolour
 # -- Iris Virginica
-data = []
-
-with open('iris.csv', 'rb') as csvfile:
-    data_set = csv.reader(csvfile, delimiter=',')
-    for sample in data_set:
-        data.append(list(map(float, sample[:4])) + [sample[4]])
-
-indices = range(len(data))
-random.shuffle(indices)
-split_pivot = int(ceil(0.2 * len(data)))   # 20 percent are test data
-test_set = map(lambda i: data[i], indices[:split_pivot])
-train_set = map(lambda i: data[i], indices[split_pivot:])
-
-k = 3
-
-
-def euclidian_distance(sample1, sample2):
-    return sqrt(sum(map(lambda t: (t[0] - t[1]) ** 2,
-                        zip(sample1[:4], sample2[:4]))))
-
-
-def predict(train_set, sample, k=3, distance=euclidian_distance):
-    return sorted(train_set, key=lambda s: distance(s, sample))[:k]
+data = read_iris('iris.csv')
+train_set, test_set = train_test_split(data, test_size=0.2, random_seed=42)
+print(accuracy(train_set, test_set, 3))
