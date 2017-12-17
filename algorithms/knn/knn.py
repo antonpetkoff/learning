d973055faa7a01731aca97852b1e36e7fd76f160
Anton Petkov <anton.petkoff@gmail.com>
2017-12-17 15:12:33 +0200

    Calculate euclidian distance

--- algorithms/knn/knn.py
+++ algorithms/knn/knn.py
@@ -1,18 +1,39 @@
 import csv
 import random
-import math
+from math import ceil, sqrt
 
 SEED = 42
 random.seed(SEED)
+
+# Attribute Information:
+# 1. sepal length in cm
+# 2. sepal width in cm
+# 3. petal length in cm
+# 4. petal width in cm
+# 5. class:
+# -- Iris Setosa
+# -- Iris Versicolour
+# -- Iris Virginica
 data = []
 
 with open('iris.csv', 'rb') as csvfile:
     data_set = csv.reader(csvfile, delimiter=',')
     for sample in data_set:
-        data.append(sample)
+        data.append(list(map(float, sample[:4])) + [sample[4]])
 
 indices = range(len(data))
 random.shuffle(indices)
-split_pivot = int(math.ceil(0.2 * len(data)))   # 20 percent are test data
-test_set = map(lambda i: data[i], indices[0:split_pivot])
+split_pivot = int(ceil(0.2 * len(data)))   # 20 percent are test data
+test_set = map(lambda i: data[i], indices[:split_pivot])
 train_set = map(lambda i: data[i], indices[split_pivot:])
+
+k = 3
+
+
+def euclidian_distance(sample1, sample2):
+    return sqrt(sum(map(lambda t: (t[0] - t[1]) ** 2,
+                        zip(sample1[:4], sample2[:4]))))
+
+
+def predict(train_set, sample, k=3, distance=euclidian_distance):
+    pass
