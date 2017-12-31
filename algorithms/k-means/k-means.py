e7a11d4767b4d43a8ef4c8d27b90cbaddcdf5e9d
Anton Petkov <anton.petkoff@gmail.com>
2017-12-31 18:21:48 +0200

    Add k-means iPython notebook

--- algorithms/k-means/k-means.py
+++ algorithms/k-means/k-means.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from __future__ import division
-import random
-from statistics import mean
-
-import seaborn as sns
-import pandas as pd
-
 from copy import deepcopy
+from matplotlib import pyplot as plt
+import random
+
+# import seaborn as sns
+# import pandas as pd
+import numpy as np
+from operator import itemgetter
+
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
 
 
 def euclidian_distance(sample1, sample2):
@@ -25,30 +28,45 @@ def euclidian_distance(sample1, sample2):
 def read_points(filename):
     with open(filename) as file:
         content = file.readlines()
-    return [list(map(float, x.strip().split('\t', 2))) for x in content]
+    return [list(map(float, x.strip().split(' ', 2))) for x in content]
 
 
-def mean(points):
+def mean(points, default_fn):
     count = len(points)
+
+    if count == 0:
+        return default_fn()
+
     sum_x1 = sum([point[0] for point in points])
     sum_x2 = sum([point[1] for point in points])
     return [sum_x1 / count, sum_x2 / count]
 
 
-def train(data_set, k, max_iterations=10, dist=euclidian_distance):
+def train(data_set, k, max_iterations=100, dist=euclidian_distance):
     # total distance between 2 vector of points
     def vector_dist(vector1, vector2):
         return sum(map(lambda t: dist(t[0], t[1]),
                        zip(vector1, vector2)))
 
+    min_x = min(data_set, key=lambda t: t[0])[0]
+    min_y = min(data_set, key=lambda t: t[1])[1]
+    max_x = max(data_set, key=lambda t: t[0])[0]
+    max_y = max(data_set, key=lambda t: t[1])[1]
+    print(min_x, min_y, max_x, max_y)
+
+    def get_random_point():
+        return [min_x + random.random() * (max_x - min_x),
+                min_y + random.random() * (max_y - min_y)]
+
     random.shuffle(data_set)
     clusters = [0 for _ in range(len(data_set))]
-    centroids = data_set[:k]    # TODO: we chose points from the data set
+    centroids = [get_random_point() for _ in range(k)]
+    # centroids = data_set[:k]    # TODO: we chose points from the data set
     centroids_old = [[0, 0] for _ in range(k)]
     error = vector_dist(centroids, centroids_old)
     iterations = 0
 
-    while error != 0 or iterations < max_iterations:
+    while error != 0 and iterations < max_iterations:
         # assign each sample to its closest cluster (nearest centroid)
         for sample_id in range(len(data_set)):
             nearest_centroid = min(centroids,
@@ -63,17 +81,30 @@ def train(data_set, k, max_iterations=10, dist=euclidian_distance):
             points = [data_set[sample_id]
                       for sample_id in range(len(data_set))
                       if clusters[sample_id] == cluster_id]
-            centroids[cluster_id] = mean(points)
+            centroids[cluster_id] = mean(points, get_random_point)
 
         error = vector_dist(centroids, centroids_old)
         iterations += 1
 
     print("finished in %d iterations" % iterations)
+    # colors = ['r', 'g', 'b', 'y', 'c', 'm']
+    # fig, ax = plt.subplots()
+    # for i in range(k):
+    #         points = np.array([data_set[j]
+    #                           for j in range(len(data_set))
+    #                           if clusters[j] == i])
+    #         ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
+    # # ax.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, c='#050505')
+    # ax.scatter(list(map(itemgetter(0), centroids)),
+    #            list(map(itemgetter(1), centroids)),
+    #            marker='*', s=200, c='#050505')
+
     return centroids
 
 
-random_seed = 42
-vectors = read_points('normal/normal.txt')
+random_seed = 2
+vectors = read_points('unbalance/unbalance.txt')
 random.seed(random_seed)
 
-print(train(vectors, 4))
+k = 8
+print(train(vectors, k))
